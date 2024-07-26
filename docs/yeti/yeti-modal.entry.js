d9841fcc6eef3d8bd328c529b67cd47c01f5e82a
msoltvedt <matt@creeksidedigital.com>
2024-07-26 07:24:19 -0500

    Tooltip: add Rich Content capability

---
+++ docs/yeti/yeti-modal.entry.js
@@ -0,0 +1,202 @@
+import { r as registerInstance, h, g as getElement } from './index-a229effc.js';
+import { u as utils } from './utils-90cea6cb.js';
+
+const YetiModal = class {
+    constructor(hostRef) {
+        registerInstance(this, hostRef);
+        this.previouslyFocusedElement = null; // So we can return focus to wherever the user was when the Loading component appeared.
+        this.bodyInnerWrapper = null; // A div wrapped around everything in the body except the modal. Used to prevent background scrolling.
+        this.shouldStealFocus = false;
+        this.shouldReturnFocus = false;
+        this.hasOpened = false;
+        this.headingId = utils.generateUniqueId();
+        this.isSideSheet = false;
+        this.heading = "Modal Heading";
+        this.describedBy = "";
+        this.size = "";
+        this.modalClass = "";
+        this.isScrollable = true;
+        this.isActive = false;
+        this.showClose = true;
+        this.isAnimating = false;
+        this.isOpening = false;
+        this.isClosing = false;
+    }
+    handleFocus(newValue) {
+        // When the Modal becomes active it should take focus away from whatever had it before, but
+        // we want to keep track of what that was so we can return focus if Modal becomes inactive.
+        // First, normal Modals have only two states: active and inactive (i.e. they don't use the isOpening and isClosing states)
+        if (!this.isSideSheet) {
+            // Becoming active, take focus
+            if (newValue) {
+                this.shouldStealFocus = true;
+                this.setBackgroundElementStyles(true);
+            }
+            // Becoming inactive, return focus
+            else {
+                this.shouldReturnFocus = true;
+                this.setBackgroundElementStyles(false);
+            }
+            // Side Sheet modals have four states: inactive, opening, active, and closing.
+        }
+        else {
+            // It's a side sheet modal
+            if (newValue) {
+                // Side sheet modal becoming active
+                this.setBackgroundElementStyles(true);
+                this.isAnimating = true;
+            }
+            else {
+                this.isAnimating = true;
+            }
+        }
+    }
+    setBackgroundElementStyles(modalIsActivating) {
+        if (modalIsActivating) {
+            document.body.classList.add("yeti-modal-has_active_modal");
+        }
+        else {
+            document.body.classList.remove("yeti-modal-has_active_modal");
+        }
+    }
+    focusTrap(e) {
+        if (this.isActive && e.keyCode == 27 && this.showClose) { // Escape button
+            this.isActive = false;
+        }
+    }
+    handleTransitionEnd(e) {
+        if (!e.propertyName || e.propertyName != "transform") {
+            return;
+        }
+        if (this.isOpening) {
+            // Becoming active, take focus
+            this.previouslyFocusedElement = document.activeElement;
+            this.handleInitialFocus();
+            this.isOpening = false;
+            this.hasOpened = true;
+        }
+        else if (this.isClosing) {
+            this.setBackgroundElementStyles(false);
+            if (this.previouslyFocusedElement) {
+                this.previouslyFocusedElement.focus();
+            }
+            this.isClosing = false;
+        }
+        this.isAnimating = false;
+    }
+    handleInitialFocus() {
+        // Sets focus on the correct thing within the modal's content when it first opens. If nothing is focusable, uses the content area itself.
+        let contentArea = this.el.querySelector('.yeti-modal-content');
+        let firstFocusableContentItem = utils.aria.focusFirstDescendant(contentArea); // either false or an HTMLElement
+        if (!firstFocusableContentItem) {
+            // There is nothing focusable within the content of the modal, so make the content area itself focusable.
+            contentArea.setAttribute("tabIndex", "-1");
+            contentArea.focus();
+        }
+    }
+    componentWillLoad() {
+        // Set up ids and handle slotted content
+        let componentId = this.el.getAttribute("id");
+        let content = this.el.querySelector("yeti-modal-content");
+        let buttons = this.el.querySelector("yeti-modal-buttons");
+        let headerAction = this.el.querySelector("yeti-modal-header-action");
+        if (!content) {
+            console.error("Yeti Modal components must have a yeti-modal-content element.");
+        }
+        else {
+            content.setAttribute("slot", "content");
+        }
+        if (!buttons) {
+            console.error("Yeti Modal components must have a yeti-modal-buttons element.");
+        }
+        else {
+            buttons.setAttribute("slot", "buttons");
+        }
+        if (headerAction) {
+            headerAction.setAttribute("slot", "header_action");
+        }
+        if (!componentId || componentId == "") {
+            componentId = utils.generateUniqueId();
+            this.el.setAttribute("id", componentId);
+        }
+        if (this.isActive) {
+            this.setBackgroundElementStyles(true);
+        }
+    }
+    render() {
+        let modalOverlayCSS = (this.isSideSheet) ? "yeti-modal_ss-overlay" : "yeti-modal-overlay";
+        let modalCSS = `yeti-modal${(this.isSideSheet) ? " yeti-modal_ss" : ""}`;
+        let modalProperties = {
+            "role": "dialog",
+            "aria-modal": "true",
+            "aria-labelledby": this.headingId
+        };
+        if (this.describedBy != "") {
+            modalProperties["aria-describedby"] = this.describedBy;
+        }
+        modalOverlayCSS += (this.isActive || this.isAnimating) ? "" : " yeti-modal-overlay__inert";
+        modalOverlayCSS += (this.isOpening) ? " yeti-modal__opening" : "";
+        modalOverlayCSS += (this.hasOpened) ? " yeti-modal__open" : "";
+        modalOverlayCSS += (this.isClosing) ? " yeti-modal__closing" : "";
+        modalCSS += (this.size == "") ? "" : ` yeti-modal-size-${this.size}`;
+        modalCSS += (this.modalClass != "") ? ` ${this.modalClass}` : "";
+        modalCSS += (this.isScrollable) ? "" : " yeti-modal__unscrollable";
+        return (h("div", { key: 'cdc2d02f3a8da4a50f1dd4a3e8b6ab483f950a5c', class: modalOverlayCSS }, h("div", { key: 'df586d5ef9ec67b160cb0f18319d3769e05d93b2', class: "yeti-modal-bumper-front", tabIndex: 0 }), h("div", Object.assign({ key: '49818303dfe6965caa9e5cf7d19adca8d591f1e1', class: modalCSS }, modalProperties), h("div", { key: '2f819849010716429715ae26100c175d5e153351', class: "yeti-modal-header" }, h("h1", { key: '8d3ea1baf65e7978d3783a5fdce6c3c5676a82db', class: "yeti-modal-header-heading", id: this.headingId }, this.heading), h("div", { key: 'debd79a8cd2bd470fbfb6551b2cdcd2017e0c9f9', class: "yeti-modal-header-action" }, h("slot", { key: '829ccdc3690c8b5bf54a7af2bcb1b198e88d4627', name: "header_action" })), (this.showClose) ?
+            h("button", { class: "yeti-modal-header-close yeti-button-ghost", onClick: () => { this.isActive = false; } }, h("span", { class: "material-icons" }, "close"))
+            :
+                null), h("div", { key: '41b6a0c15fe03faf87269551f22be510b10abc28', class: "yeti-modal-content" }, h("div", { key: 'e7a4395c14bdc7ed8196e65460a9809d389b4ee8', class: "yeti-modal-content-actual" }, h("slot", { key: '91ea6a58e2ab06a12be4106082021ef9bd986933', name: "content" })), h("div", { key: 'db1dfe4cfcda6d2b8c078a46d273a1d63f8b0f71', class: "yeti-modal-content-fade" })), h("div", { key: '78fa9a73883e5c9628b5ec37e88dd814f3894c7e', class: "yeti-modal-actions" }, h("div", { key: '76df8f9d439cc748a8e81b396c440b80575a4dcb', class: "yeti-modal-actions-buttons" }, h("slot", { key: '65f0200212b38cc02d166a311391a2222f5fde61', name: "buttons" })))), h("div", { key: 'b5fffabd1dd8a418aa6f5b5b7cde6c2610a5997f', class: "yeti-modal-bumper-back", tabIndex: 0 })));
+    }
+    componentDidRender() {
+        // Handle focus management. We can't do this when the property changes because the inactive state uses display: none,
+        // which interferes with the ability to accept focus depending on some race conditions.
+        // For regular modals...
+        if (this.shouldStealFocus) {
+            // Becoming active, take focus
+            this.previouslyFocusedElement = document.activeElement;
+            this.handleInitialFocus();
+        }
+        if (this.shouldReturnFocus) {
+            // Becoming inactive, return focus
+            if (this.previouslyFocusedElement) {
+                this.previouslyFocusedElement.focus();
+            }
+        }
+        // For side sheet modals
+        if (this.isAnimating) {
+            // It's a side sheet modal, and it's opening. Add the opening class to the overlay to initiate the CSS transition.
+            if (this.isActive) {
+                setTimeout(() => {
+                    this.isOpening = true;
+                    this.isClosing = false;
+                }, 1);
+            }
+            else {
+                setTimeout(() => {
+                    this.isOpening = false;
+                    this.hasOpened = false;
+                    this.isClosing = true;
+                }, 1);
+            }
+        }
+        this.shouldStealFocus = false;
+        this.shouldReturnFocus = false;
+    }
+    componentDidLoad() {
+        let frontBumper = this.el.querySelector(".yeti-modal-bumper-front");
+        let backBumper = this.el.querySelector(".yeti-modal-bumper-back");
+        frontBumper.addEventListener("focus", () => {
+            utils.aria.focusLastDescendant(this.el.querySelector(".yeti-modal"));
+        });
+        backBumper.addEventListener("focus", () => {
+            utils.aria.focusFirstDescendant(this.el.querySelector(".yeti-modal"));
+        });
+    }
+    get el() { return getElement(this); }
+    static get watchers() { return {
+        "isActive": ["handleFocus"]
+    }; }
+};
+
+export { YetiModal as yeti_modal };
+
+//# sourceMappingURL=yeti-modal.entry.js.map
